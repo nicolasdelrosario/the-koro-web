4573ffa7b1fc1061b836d5cfde8199c2da8aaf71
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-11-21 17:39:43 -0500

    feat(icons): :sparkles: add x-icon component for ui usage

---
+++ components/icons/x-icon.tsx
@@ -0,0 +1,19 @@
+export default function XIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="#2E2E2E"
+      strokeWidth={1}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="lucide lucide-x-icon lucide-x"
+    >
+      <title>X</title>
+      <path d="M18 6 6 18" />
+      <path d="m6 6 12 12" />
+    </svg>
+  );
+}
